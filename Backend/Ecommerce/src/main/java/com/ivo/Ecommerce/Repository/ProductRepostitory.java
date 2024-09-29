0c5df5ec5fd8364eb6efa96df05cf854daebed7c
TuanThanh0611 <nguyenhatuanthanh1901@gmail.com>
2024-09-29 22:32:31 +0700

    create service to call rest Api, update angular component to subsc data from angular service, display the data in an html page

--- Backend/Ecommerce/src/main/java/com/ivo/Ecommerce/Repository/ProductRepostitory.java
+++
@@ -1,7 +0,0 @@
-package com.ivo.Ecommerce.Repository;
-
-import com.ivo.Ecommerce.Entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepostitory extends JpaRepository<Product,Long> {
-}
--- Frontend/Angular-Ecommerce/src/app/components/product-list/product-list.component.ts
+++ Frontend/Angular-Ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../common/product';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
+  products:Product[]=[];
+  constructor(private productServide:ProductService){}
+  ngOnInit(): void {
+      
+  }
+  listProducts(){
+    this.productServide.getProductList().subscribe(
+      data=>{
+        this.products=data;
+      }
+    )
+  }
 
 }

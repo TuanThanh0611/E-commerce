129192e43b45663424e02ae583727f8b9e047a4a
TuanThanh0611 <nguyenhatuanthanh1901@gmail.com>
2024-10-03 23:07:44 +0700

    Update

--- Frontend/Angular-Ecommerce/src/app/components/product-list/product-list.component.ts
+++ Frontend/Angular-Ecommerce/src/app/components/product-list/product-list.component.ts
@@ -3,22 +3,26 @@ import { ProductService } from '../../services/product.service';
 import { Product } from '../../common/product';
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
+  templateUrl: './product-list-table.component .html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent implements OnInit{
-  products:Product[]=[];
-  constructor(private productService:ProductService){}
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) {}
+
   ngOnInit(): void {
-    this.listProducts(); //lack of this code is nguồn cơn tai hại(nhìn mòn mắt 3 tiếng đồng hồ)
-      
-  }
-  listProducts(){
-    this.productService.getProductList().subscribe(
-      data=>{
-        this.products=data;
-      }
-    )
+    this.listProducts(); // Gọi phương thức để lấy danh sách sản phẩm
   }
 
+  listProducts() {
+    this.productService.getProductList().subscribe(data => {
+      this.products = data;
+
+      // Ghi lại đường dẫn hình ảnh vào console
+      this.products.forEach(product => {
+        console.log(product.imageUrl); // Ghi đường dẫn hình ảnh
+      });
+    });
+  }
 }
